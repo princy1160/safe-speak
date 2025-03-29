d25293f16f8b2336442cc5a6c6fbd1b1ce8421b3
Princy Patwa <147512280+princy1160@users.noreply.github.com>
2025-03-29 23:22:46 +0530

    Create 0001_giant_lizard.sql

---
+++ migrations/meta/0001_giant_lizard.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "messages" ADD COLUMN "user_id" integer NOT NULL;--> statement-breakpoint
+ALTER TABLE "messages" ADD CONSTRAINT "messages_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
